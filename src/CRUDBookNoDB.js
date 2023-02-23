6aa67b0c8c902bc579dfc24692857993d93cbab3
POKPONG012 <s6406021632040@email.kmutnb.ac.th>
2023-02-23 08:27:40 +0700

    t4

--- src/CRUDBookNoDB.js
+++ src/CRUDBookNoDB.js
@@ -36,8 +36,8 @@ app.get('/books',(req,res) => {
 //route to get a book by id
 app.get('/books/:id' , (req,res) =>{
     const book = books.find(b => b.id === parseInt(req.params.id));
-    if(!book) res.status(404).send('Book not found');
-    res.json(book);
+    if(!book) res.status(404).send('Book not found  ');
+    res(book);
 });
 
 //route to create a new book
